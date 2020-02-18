4125a990004dde62279550c089128ccf138b8129
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-18 22:05:31 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
-def my_each(array)
-array = []
+def my_each(array) # put argument(s) here
+
 while array.length < 4
-  my_each.each do |i|
-     puts i
+  my_each(collection) do |i|
+     i
   end
 end
